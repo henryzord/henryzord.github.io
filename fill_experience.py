a43c0fad96d864b3a470032a63d8e8d91b21cf27
henryzord <henry.cagnini@gmail.com>
2023-08-13 08:21:19 -0300

    fixed path of file and added navbar to templates

--- fill_experience.py
+++ fill_experience.py
@@ -1,18 +1,17 @@
 import os
 
-from jinja2 import Environment, FileSystemLoader
-import click
-import yaml
+import jinja2
 
 
 def main():
-    env = Environment(loader=FileSystemLoader('templates/'))
-    template = env.get_template('template_experience.html')
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates/'))
+    experience_template = env.get_template('template_experience.html')
+    navbar = env.get_template('navigation_bar.html')  # type: jinja2.Template
 
     with open(os.path.join('resources', 'lattes_graph.html'), 'r', encoding='utf-8') as read_file:
-        code = read_file.read()
+        lattes_graph = read_file.read()
 
-    content = template.render(lattes_graph=code)
+    content = experience_template.render(lattes_graph=lattes_graph, navigation_bar=navbar.render())
     with open('experience.html', 'w', encoding='utf-8') as write_file:
         write_file.write(content)
 
